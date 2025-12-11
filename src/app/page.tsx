d56b4272b1d56b9588397cfdc4b678198fff587a
Zeks92 <zeks1992@gmail.com>
2025-12-11 16:37:05 +0100

    Started working on stepper, step 1 ready

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
-import Box from '@mui/material/Box';
-import Footer from './components/Footer';
-import Header from './components/Header';
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import MatchFinderStepper from "./components/MatchFinderStepper";
 
 export default function HomePage() {
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+    <Box sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
       <Header />
+      <Container
+        component="main"
+        maxWidth="lg"
+        sx={{ mt: 4, mb: 4, flexGrow: 1 }}
+      >
+        <MatchFinderStepper />
+      </Container>
       <Footer />
     </Box>
   );
